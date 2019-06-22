b36042eecbd63c33e282d0a55bb57b46f6316df0
ameddin73 <ameddin73@gmail.com>
2019-06-22 16:53:45 -0400

    rest endpoint security

--- src/main/java/com/bikeshare/worldmap/repository/ProgramRepository.java
+++ src/main/java/com/bikeshare/worldmap/repository/ProgramRepository.java
@@ -2,8 +2,32 @@ package com.bikeshare.worldmap.repository;
 
 import com.bikeshare.worldmap.model.Program;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProgramRepository extends CrudRepository<Program, Long> {
+    @Override
+    @RestResource(exported = false)
+    <S extends Program> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Program> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Program entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Program> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
