86ed4e276c5dbf9e42b6830fe1e55072e94f1673
ameddin73 <ameddin73@gmail.com>
2019-06-26 19:14:17 -0400

    trying to make a list of programs...

--- src/main/java/com/bikeshare/worldmap/services/GoogleSheetsService.java
+++ src/main/java/com/bikeshare/worldmap/services/GoogleSheetsService.java
@@ -21,8 +21,12 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.io.*;
 import java.security.GeneralSecurityException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.*;
 
 @Component
 public class GoogleSheetsService {
@@ -54,22 +58,76 @@ public class GoogleSheetsService {
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
-    @PostConstruct
-    private static List<Program> getSheetAsList() throws IOException, GeneralSecurityException {
-        log.info("Starting sheets service...");
+    private static List<List<Object>> getSheetAsList() throws IOException, GeneralSecurityException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME).build();
         ValueRange response = service.spreadsheets().values().get(SPREADSHEET_ID,RANGE).execute();
-        List<List<Object>> values = response.getValues();
-        if (values == null || values.isEmpty()) {
-            System.out.println("No data found");
-        } else {
-            System.out.println("List: ");
-            for (List row: values) {
-                System.out.println(row.toString());
+        return response.getValues();
+    }
+
+    private static Program getProgram(List<Object> objects) {
+        String city = objects.get(0).toString();
+        String country = objects.get(1).toString();
+        String continent = objects.get(2).toString();
+        String name = objects.get(3).toString();
+        Integer status = Integer.valueOf(objects.get(4).toString());
+        Date startDate = null;
+        Date endDate = null;
+        try {
+            String sDate = objects.get(5).toString();
+            String eDate = objects.get(6).toString();
+            if (sDate.length() == 4) {
+                startDate = new SimpleDateFormat("yyyy").parse(sDate);
+            } else {
+                startDate = new SimpleDateFormat("yyyy-MM-dd").parse(sDate);
             }
+            if (eDate.length() == 4) {
+                endDate = new SimpleDateFormat("yyyy").parse(eDate);
+            } else {
+                endDate = new SimpleDateFormat("yyyy-MM-dd").parse(eDate);
+            }
+        } catch (ParseException e) {
+            log.error("Error parsing date: " + e.getMessage());
         }
-        return null;
+        String url = objects.get(7).toString();
+        String latLong = objects.get(8).toString();
+
+        String[] csv = latLong.split(",");
+        Float longitude = Float.valueOf(csv[0]);
+        Float latitude = Float.valueOf(csv[1]);
+
+        return new Program(city, continent, country, endDate, latitude, longitude,
+                name, startDate, status, url);
+    }
+
+    private static Runnable updateData() {
+        return new Runnable() {
+            List<List<Object>> values;
+            List<Program> programs;
+            @Override
+            public void run() {
+                try {
+                    values = getSheetAsList();
+                } catch (IOException e) {
+                    log.error("IO error reading google sheets: " + e.getMessage());
+                } catch (GeneralSecurityException e) {
+                    log.error("Security error reading google sheets: " + e.getMessage());
+                }
+
+                for (List<Object> objects : values)
+                    programs.add(getProgram(objects));
+
+                for (Program program : programs)
+                    System.out.println(program.toString());
+            }
+        };
+    }
+
+    @PostConstruct
+    private static void runDataUpdate() {
+        log.info("Initializing Google Sheets scheduled service.");
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(updateData(),0,1, TimeUnit.DAYS);
     }
 }
