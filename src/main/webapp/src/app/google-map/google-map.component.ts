384a7fb955ced6f2cbc007a30c9fee74bb7c97a4
ameddin73 <ameddin73@gmail.com>
2019-08-25 16:59:30 -0400

    display and load markers in google map. I did it!!!!

--- src/main/webapp/src/app/google-map/google-map.component.ts
+++ src/main/webapp/src/app/google-map/google-map.component.ts
@@ -1,55 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MouseEvent} from "@agm/core";
+import {Program} from "../models/program.model";
+import {ProgramService} from "../shared/program/program.service";
 
 @Component({
   selector: 'app-google-map',
   templateUrl: './google-map.component.html',
   styleUrls: ['./google-map.component.css']
 })
-export class GoogleMapComponent {
+export class GoogleMapComponent implements OnInit{
+  programs: Array<Program>;
+
   // google maps zoom level
   zoom: number = 8;
 
   // initial center position for the map
-  lat: number = 51.673858;
-  lng: number = 7.815982;
+  lat: number = 39.9526;
+  lng: number = -75.1652;
 
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
-  }
+  //markers
+  markers: marker[];
 
-  mapClicked($event: MouseEvent) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
+  constructor(private programService: ProgramService) {}
+
+  ngOnInit() {
+    this.programService.getAll().subscribe(data => {
+      this.programs = data._embedded.programs;
+      this.programs.sort(((a, b) => {return (a.startDate<b.startDate?1:-1)}));
+      this.markers = this.programs.map((program) => {
+        return {
+          lat: program.latitude,
+          lng: program.longitude,
+          label: program.name,
+          city: program.city
+        };
+      });
     });
   }
-
-  markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
-  }
-
-  markers: marker[] = [
-    {
-      lat: 51.673858,
-      lng: 7.815982,
-      label: 'A',
-      draggable: true
-    },
-    {
-      lat: 51.373858,
-      lng: 7.215982,
-      label: 'B',
-      draggable: false
-    },
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-      draggable: true
-    }
-  ]
 }
 
 // just an interface for type safety.
@@ -57,5 +44,5 @@ interface marker {
   lat: number;
   lng: number;
   label?: string;
-  draggable: boolean;
+  city: string;
 }
